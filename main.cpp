753a14a21ad2835d4ff6b794c0570751081f8397
vinayintipalli <vinayintipalli@gmail.com>
2024-10-02 21:28:04 +0530

    Enhanced the code with lock-free programming techniques

--- main.cpp
+++ main.cpp
@@ -1,42 +1,53 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <shared_mutex>
-#include <mutex>
+#include <atomic>
 
 class BankingSystem {
 public:
     explicit BankingSystem(int initial_balance) : balance(initial_balance) {}
 
     void deposit(int amount) {
-        std::unique_lock<std::shared_mutex> ul(balance_mutex);
-        balance += amount;
+        int currentBalance = balance.load();
+        int updatedBalance = currentBalance + amount;
+        while(not balance.compare_exchange_weak(currentBalance, updatedBalance)) {
+            updatedBalance = currentBalance + amount;    
+        }
+        // compare_exchange_weak attempts to atomically set balance to updatedBalance, 
+        // but only if balance is still equal to currentBalance. 
+        // If balance was changed by another thread in the meantime, the operation will fail, 
+        // and currentBalance will be updated with the current value of balance.
     }
 
     void withdraw(int amount) {
-        std::unique_lock<std::shared_mutex> ul(balance_mutex);
-        if (balance >= amount) {
-            balance -= amount;
-        } else {
-            std::cout << "Insufficient balance for withdrawal!\n";
+        int currentBalance = balance.load();
+        int updatedBalance = currentBalance - amount;
+        if (currentBalance < amount) {
+            std::cout << "Insufficient balance for withdrawal!";
+            return;
+        }
+        while(not balance.compare_exchange_weak(currentBalance, updatedBalance)) {
+            if (currentBalance < amount) {
+                std::cout << "Insufficient balance for withdrawal!";
+                return;
+            }
+            updatedBalance = currentBalance - amount;    
         }
     }
 
     void printBalance() const {
-        std::shared_lock<std::shared_mutex> sm(balance_mutex);
-        std::cout << "Final Balance: " << balance << std::endl;
+        std::cout << "Final Balance: " << balance.load() << std::endl;
     }
 
 private:
-    int balance;
-    mutable std::shared_mutex balance_mutex;
+    std::atomic<int> balance;
 };
 
 int main() {
     BankingSystem bankingSystem(0);
     std::vector<std::thread> threads;
 
-    while (threads.size() <= 10) {
+    while (threads.size() <= 20) {
         threads.emplace_back(&BankingSystem::deposit, &bankingSystem, 10);
         threads.emplace_back(&BankingSystem::withdraw, &bankingSystem, 10);
     }
@@ -46,6 +57,5 @@ int main() {
     }
 
     bankingSystem.printBalance();
-
     return 0;
 }
